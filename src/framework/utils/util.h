0ec8f96c62dd842f62c0d71820640129e2667090
wxd <wxdwfc@gmail.com>
2018-03-05 11:37:15 +0800

    refine random number

--- src/framework/utils/util.h
+++ src/framework/utils/util.h
@@ -306,6 +306,17 @@ namespace nocc {
       {
         this->seed = (seed ^ 0x5DEECE66DL) & ((1L << 48) - 1);
       }
+
+      inline uint64_t rand_number(int min,int max) {
+        return check_between_inclusive( (uint64_t)(next_uniform() * (max - min + 1) + min),min,max);
+      }
+
+      inline uint64_t check_between_inclusive(uint64_t v,uint64_t min,uint64_t max) {
+        assert(v >= min);
+        assert(v <= max);
+        return v;
+      }
+
     private:
 
       inline unsigned long
